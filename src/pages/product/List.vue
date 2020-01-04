0bb73514836ebf186e31eaa063ec7ee3ab418dd6
694270430 <wuyongkaikai@163.com>
2020-01-04 13:58:48 +0800

    kk2

--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -34,9 +34,7 @@
         :visible.sync="visible"
         width="60%">
         <el-form :model="form" label-width="80px">
-            <el-form-item label="编号">
-                <el-input v-model="form.id"/>
-            </el-form-item>
+            
             <el-form-item label="产品名称">
                 <el-input v-model="form.name"/>
             </el-form-item>
@@ -66,6 +64,10 @@ import request from'@/utils/request'
 import querystring from 'querystring'
 export default {
   // 用于存放网页中需要调用的方法
+  created(){
+          //vue实例创建完毕
+          this.loadData();
+        },
   methods:{
     
     loadData(){
@@ -110,7 +112,7 @@ export default {
             this.loadData();
               this.$message({
               type: 'success',
-              message:response.message
+              message:"删除成功"
             });
 
           })
@@ -129,7 +131,7 @@ export default {
       },
       toAddHandler(){
         this.visible = true;
-        this.form = {
+        this.form={
           type:"product"
         }
       }
@@ -144,10 +146,7 @@ export default {
           }
         }
       },
-        created(){
-          //vue实例创建完毕
-          this.loadData();
-        }
+        
     }
 </script>
 <style scoped>
